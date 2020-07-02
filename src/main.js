fb7b249577d62583b77fc07e99e124e1e3aadb6e
Tobey <bitsqiu@gmail.com>
2020-07-02 22:19:34 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import http from "./http";
+// 所有网络请求通过 this.$http 调用
+Vue.prototype.$http = http;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
