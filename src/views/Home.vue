5054d3cc3ac80fb83bbc1f219f063b3003b4d9e4
Tobey <bitsqiu@gmail.com>
2020-07-03 10:30:52 +0800

    添加首页页面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-container">
+    <el-card header="【首页】" class="home-card">
+      <el-form @submit.native.prevent="enterTheGame">
+        <el-form-item label="名字">
+          <el-input v-model="model.username"></el-input>
+        </el-form-item>
+        <el-form-item label="房间号">
+          <el-input v-model="model.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" native-type="submit">进入游戏</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    enterTheGame() {
+      console.log(this.model);
+    },
+  },
+};
 </script>
+
+<style>
+.home-card {
+  width: 25rem;
+  margin: 6rem auto;
+}
+</style>
