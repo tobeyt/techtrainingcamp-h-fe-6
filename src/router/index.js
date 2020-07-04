d0d826ea07eeb85a0d1d8447897921950bd1949c
unknown <1227382759@qq.com>
2020-07-04 20:30:09 +0800

    添加创建房间页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Room from '../views/Room.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ Vue.use(VueRouter)
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/room',
+    name: 'Room',
+    component: Room
   }
 ]
 
---
+++ src/views/Room.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="room-container">
+
+    
+    <el-card class="home-card">
+      <el-row  type="flex" class="row-bg title" >
+        <i class="el-icon-s-home"> [创建房间] </i>
+      </el-row >
+      <el-form @submit.native.prevent="enterTheRoom">
+
+        <el-row type="flex" class="row-bg" justify="center">
+          <el-form-item label="房间号" v-model="model.password" class="font">00000
+          </el-form-item>
+        </el-row>
+
+        <el-row type="flex" class="row-bg" justify="center">
+          <el-button type="primary" native-type="submit" class="button"
+            >创建房间</el-button>
+        </el-row>
+
+      </el-form>
+
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    enterTheRoom() {
+      console.log(this.model);
+    },
+  },
+};
+</script>
+<style>
+.title{
+  color: #7babeb;
+}
+.font{
+  color:#F56C6C;
+}
+</style>
