33c32940f1860c42bc0ab99fded2148949d71f4e
unknown <1227382759@qq.com>
2020-07-06 18:36:40 +0800

    修改创建房间页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,7 +36,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .home-card {
   width: 25rem;
   margin: 6rem auto;
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="room-container">
-
-    
-    <el-card class="home-card">
+    <el-card class="room-card">
       <el-row  type="flex" class="row-bg title" >
         <i class="el-icon-s-home"> [创建房间] </i>
       </el-row >
-      <el-form @submit.native.prevent="enterTheRoom">
-
-        <el-row type="flex" class="row-bg" justify="center">
-          <el-form-item label="房间号" v-model="model.password" class="font">00000
-          </el-form-item>
-        </el-row>
-
-        <el-row type="flex" class="row-bg" justify="center">
-          <el-button type="primary" native-type="submit" class="button"
-            >创建房间</el-button>
-        </el-row>
-
-      </el-form>
-
+        <div class="show-room">
+          <h4>您的房间号是:</h4>
+          <h2>{{ id }}</h2>
+        </div>
+        <p class="wait" v-show="flag">正在等待其他玩家进入...</p>
+        <div class="create">
+          <el-button type="primary" native-type="submit" class="button" @click="getRoomId(len=4)">创建房间</el-button>
+        </div>
     </el-card>
   </div>
 </template>
@@ -27,21 +19,29 @@
 export default {
   data() {
     return {
-      model: {},
+      id: "0000",
+      flag:false,
     };
   },
   methods: {
-    enterTheRoom() {
-      console.log(this.model);
+    //生成四位不重复的随机房间号
+    getRoomId(length) {
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    this.id =  arr.sort(() => Math.random() - 0.5).slice(0, length).join("")
+    this.flag = true
+    console.log(this.id);
     },
   },
 };
 </script>
-<style>
+<style scoped>
 .title{
   color: #7babeb;
 }
-.font{
+.show-room,.create,.wait{
+  text-align: center;
+}
+h2{
   color:#F56C6C;
 }
 </style>
