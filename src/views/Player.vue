3d6eb2e051745f2604c8d561491e9295648b1268
smallbrave <1060401583@qq.com>
2020-07-07 15:49:41 +0800

    使用节流函数

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -3,27 +3,28 @@
       <el-card  header="【角色展示页】" class="player-card">
           <div class="show-role">
               <h4>您当前的角色是：</h4>
-              <h2>{{ role }}</h2>
+              <h2 v-loading="loading">{{ role }}</h2>
           </div>
           <div class="status">
               <p>状态: {{ status }}</p>         
           </div>
           <div class="handle">
-              <el-button size="mini" @click="refresh">状态刷新</el-button>
-              <el-button size="mini" @click="getResult">查看结果</el-button>
+              <el-button size="mini" @click="genxin">状态刷新</el-button>
+              <el-button size="mini" @click="getResult" :type="!status?'success':''">查看结果</el-button>
           </div>
       </el-card>
   </el-container>
 </template>
 <script>
+import { throttle } from  "../common"
 export default {
-    // 设置默认提交分支
     data(){
         return {
             role: "",
             status: "游戏中",
             name: "",
             roomId: "",
+            loading: true
         }
     },
     created() {
@@ -33,38 +34,49 @@ export default {
     },
     methods:{
         //获取当前角色
-        getRoleAndStatus(){
-            this.$http(`getRoleAndStatus?name=${this.name}&roomid=${this.roomId}`)
-            .then((res)=>{
-                if(res.status == 200){
-                    this.setRoleAndStatus(res.data)
-                }
-            }).catch((reason)=>{
-                console.log(reason)
-            })
+        async getRoleAndStatus(){
+            let result = await this.$http(`getRoleAndStatus?name=${this.name}&roomid=${this.roomId}`)
+            if(result.status == 200){
+                let {data} = result;
+                this.setRoleAndStatus(data);
+                this.loading = false;
+            }else {
+                this.loading = false;
+                this.role = "身份加载失败";                
+                this.$message.error("网络错误");
+            }
         },
         // 状态刷新
-        refresh() {
-            this.getRoleAndStatus();
-            console.log("状态刷新")
-        },
+        // async refresh() {
+            
+        // },
 
+        genxin: throttle(async function(){
+                await this.getRoleAndStatus();
+                this.$message({
+                    message: "状态已刷新",
+                    type: "success",
+                    duration: 1000
+                })
+        },1000),
+        
         //查看比赛结果
         getResult() {
-            console.log("查看结果")
+            console.log("查看结果");
         },
 
         // 设置角色和状态
         setRoleAndStatus(data){
             this.role = data.role;
             if(data.status){
-                this.status = "游戏中"
+                this.status = "游戏中";
             }else {
-                this.status = "已死"
+                this.status = "已死";
             }
-        }
-
-
+        },
+        
+        // 节流函数
+        
     }
 }
 </script>
@@ -81,6 +93,7 @@ export default {
 }
 
 .show-role h2 {
+    height: 40px;
     color: purple;
 }
 </style>
