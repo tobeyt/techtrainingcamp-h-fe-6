448ca2e7f87f7cdceb5988f06644ddeebe158cc0
Tobey <bitsqiu@gmail.com>
2020-07-10 10:40:57 +0800

    add base url

--- src/http.js
+++ src/http.js
@@ -3,7 +3,7 @@ import Vue from "vue";
 import router from "./router";
 
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || "/admin/api",
+  baseURL: "https://afxltd.fn.thelarkcloud.com/",
 });
 
 http.interceptors.request.use(
