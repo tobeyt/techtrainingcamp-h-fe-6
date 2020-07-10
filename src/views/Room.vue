b7d50ae3df9194345f47a103f7e5a16a9567d846
Tobey <bitsqiu@gmail.com>
2020-07-10 10:32:28 +0800

    add baseURL

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="room-container">
     <el-card class="card">
-      <el-row  type="flex" class="row-bg title" >
+      <el-row type="flex" class="row-bg title">
         <i class="el-icon-s-home"> [创建房间] </i>
-      </el-row >
-        <div class="show-room">
-          <h4>您的房间号是:</h4>
-          <h2>{{ id }}</h2>
-        </div>
-        <p class="wait" v-show="flag">正在等待其他玩家进入...</p>
-        <div class="create">
-          <el-button type="primary" native-type="submit" class="button" @click="getRoomId(len=4)">创建房间</el-button>
-        </div>
+      </el-row>
+      <div class="show-room">
+        <h4>您的房间号是:</h4>
+        <h2>{{ id }}</h2>
+      </div>
+      <p class="wait" v-show="flag">正在等待其他玩家进入...</p>
+      <div class="create">
+        <el-button
+          type="primary"
+          native-type="submit"
+          class="button"
+          @click="getRoomId((len = 4))"
+          >创建房间</el-button
+        >
+      </div>
     </el-card>
   </div>
 </template>
@@ -20,22 +26,29 @@ export default {
   data() {
     return {
       id: "0000",
-      flag:false,
+      flag: false,
     };
   },
   methods: {
     //生成四位不重复的随机房间号
     async getRoomId(length) {
-      let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-      this.id =  arr.sort(() => Math.random() - 0.5).slice(0, length).join("")
-      this.flag = true
+      let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      this.id = arr
+        .sort(() => Math.random() - 0.5)
+        .slice(0, length)
+        .join("")
+        .toString();
+      this.flag = true;
       console.log(this.id);
-      const res = await this.$http.post("https://afxltd.fn.thelarkcloud.com/createRoom", this.id);
+      const res = await this.$http.post(
+        "createRoom",
+        { roomid: this.id }
+      );
       console.log(res);
-      if(res.data.code == 422){
-        console.log('房间号已存在')
-      }else if(res.data.code == 200){
-        console.log('创建房间成功')
+      if (res.data.code == 422) {
+        console.log("房间号已存在");
+      } else if (res.data.code == 200) {
+        console.log("创建房间成功");
       }
       // this.$router.push("/gud");
       // this.$message({
@@ -47,13 +60,15 @@ export default {
 };
 </script>
 <style>
-.title{
+.title {
   color: #7babeb;
 }
-.show-room,.create,.wait{
+.show-room,
+.create,
+.wait {
   text-align: center;
 }
-h2{
-  color:#F56C6C;
+h2 {
+  color: #f56c6c;
 }
 </style>
