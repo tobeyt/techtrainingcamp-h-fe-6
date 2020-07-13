99197c1b27bd7e796f1b9df46be8b92a1a1b6ae4
webaidx <1227382759@qq.com>
2020-07-13 20:01:52 +0800

    添加了playNumber数据请求以及获取加入房间人数功能

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -3,19 +3,39 @@
     <el-card class="card">
       <el-row type="flex" class="row-bg title">
         <i class="el-icon-s-home"> [创建房间] </i>
+        <el-dropdown>
+        <span class="el-dropdown-link row-bg" type="flex" justify="end">
+          参加游戏人数<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item @click.native="getPlayerNumber(num = 6)">6</el-dropdown-item>
+          <el-dropdown-item @click.native="getPlayerNumber(num = 7)">7</el-dropdown-item>
+          <el-dropdown-item @click.native="getPlayerNumber(num = 8)">8</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
       </el-row>
+
       <div class="show-room">
         <h4>您的房间号是:</h4>
         <h2>{{ id }}</h2>
       </div>
-      <p class="wait" v-show="flag">正在等待其他玩家进入...</p>
+      <p class="wait" v-show="flag">正在等待其他玩家进入（ <span>{{enterNumber}}</span> 人 )</p>
       <div class="create">
         <el-button
           type="primary"
           native-type="submit"
           class="button"
-          @click="getRoomId((len = 4))"
+          @click="getRoomId(len = 4,num = 0)"
+          v-show="!flag"
           >创建房间</el-button
+        >
+        <el-button
+          type="primary"
+          native-type="submit"
+          class="button"
+          v-show="mark"
+          @click="startPlay"
+          >开始游戏</el-button
         >
       </div>
     </el-card>
@@ -27,6 +47,9 @@ export default {
     return {
       id: "0000",
       flag: false,
+      mark:false,
+      playNumber: 0,
+      enterNumber:0,
     };
   },
   methods: {
@@ -36,26 +59,45 @@ export default {
       this.id = arr
         .sort(() => Math.random() - 0.5)
         .slice(0, length)
-        .join("")
-        .toString();
+        .join("");
       this.flag = true;
-      console.log(this.id);
+      // console.log(this.id,this.playNumber);
       const res = await this.$http.post(
         "createRoom",
-        { roomid: this.id }
+        { roomid: this.id,
+          playNumber: this.playNumber
+        }
       );
-      console.log(res);
-      if (res.data.code == 422) {
-        console.log("房间号已存在");
-      } else if (res.data.code == 200) {
-        console.log("创建房间成功");
-      }
-      // this.$router.push("/gud");
-      // this.$message({
-      //   type: "success",
-      //   message: "创建房间成功",
-      // });
-    },
+      // console.log(res);
+      if (res.status !== 200) return this.$message.error('创建房间失败')
+        await this.$message.success('创建房间成功')
+
+        //轮询显示进入房间人数
+        let timer = setInterval(res=> {
+          this.$http.get(
+            "getPlayCount",
+          )
+          this.enterNumber = res.data.data.count;
+          console.log(res.data.data.count);
+        },6000);
+
+        //判断房间人数是否已满
+        if(this.playNumber == this.enterNumber){
+          clearInterval(timer);
+          this.mark = !this.mark;
+        } 
+    },  
+
+    //获取选择的房间人数
+    getPlayerNumber(number) {
+      this.playNumber = number;
+      console.log(this.playNumber);
+     },
+
+     //开始游戏
+     startPlay() {
+       this.$router.push({path:'/gud', query:{roomid:this.id}})
+     }
   },
 };
 </script>
@@ -70,5 +112,15 @@ export default {
 }
 h2 {
   color: #f56c6c;
+}
+.el-dropdown {
+    right: -55%;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
 }
 </style>
